3adbf1164c0908a14e27d99ca95e3990097b0c6a
courtneypattison <courtney@courtneypattison.com>
2018-01-21 13:32:07 -0800

    Generated TimePipe

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TimePipe } from './time.pipe';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [TimePipe]
 })
 export class SharedModule { }
